cfd216bc328db6fbac9067326615c6094a47bb80
Michael Shimmins <michael@teamsquare.co>
2016-10-31 23:38:53 +1100

    Added coverage for logout method

--- spec/s2_netbox/authentication_spec.rb
+++ spec/s2_netbox/authentication_spec.rb
@@ -2,21 +2,39 @@ require 'spec_helper'
 
 describe S2Netbox::Authentication do
 
-  before :each do
-    configure
+  describe 'login' do
+    before :each do
+      configure
 
-    stub_request(:post, "https://test-s2.some.net/goforms/nbapi").
-        with(:body => "APIcommand=<NETBOX-API><COMMAND name='Login' num='1'><PARAMS><USERNAME>some_password</USERNAME><PASSWORD>some_password</PASSWORD></PARAMS></COMMAND></NETBOX-API>").
-        to_return(:status => 200, :body => "<NETBOX sessionid='255385874'><RESPONSE command='Login' num='1'><CODE>SUCCESS</CODE></RESPONSE></NETBOX>", :headers => {})
+      stub_request(:post, "https://test-s2.some.net/goforms/nbapi").
+          with(:body => "APIcommand=<NETBOX-API><COMMAND name='Login' num='1'><PARAMS><USERNAME>some_password</USERNAME><PASSWORD>some_password</PASSWORD></PARAMS></COMMAND></NETBOX-API>").
+          to_return(:status => 200, :body => "<NETBOX sessionid='255385874'><RESPONSE command='Login' num='1'><CODE>SUCCESS</CODE></RESPONSE></NETBOX>", :headers => {})
 
-    @result = S2Netbox::Authentication.login
+      @result = S2Netbox::Authentication.login
+    end
+
+    it 'should be a S2Netbox::ApiResponse' do
+      expect(@result).to be_a(S2Netbox::ApiResponse)
+    end
+
+    it 'should return the correct session_id' do
+      expect(@result.session_id).to eq('255385874')
+    end
   end
 
-  it 'should be a S2Netbox::ApiResponse' do
-    expect(@result).to be_a(S2Netbox::ApiResponse)
-  end
+  describe 'logout' do
+    before :each do
+      configure
 
-  it 'should return the correct session_id' do
-    expect(@result.session_id).to eq('255385874')
+      stub_request(:post, "https://test-s2.some.net/goforms/nbapi").
+          with(:body => "APIcommand=<NETBOX-API sessionid='255385874'><COMMAND name='Logout' num='1'></COMMAND></NETBOX-API>").
+          to_return(:status => 200, :body => "<NETBOX><RESPONSE command='Logout' num='1'><CODE>SUCCESS</CODE></RESPONSE></NETBOX>", :headers => {})
+
+      @result = S2Netbox::Authentication.logout('255385874')
+    end
+
+    it 'should be a S2Netbox::ApiResponse' do
+      expect(@result).to be_a(S2Netbox::ApiResponse)
+    end
   end
 end
