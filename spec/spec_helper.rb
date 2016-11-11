189a334f949816f010a674dc32397dc2d6517735
Michael Shimmins <michael@teamsquare.co>
2016-11-11 20:18:55 +1100

    Revert a few changes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,10 @@
 require 'simplecov'
 
 if ENV['CIRCLE_ARTIFACTS']
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  dir = File.join("..", "..", "..", ENV['CIRCLE_ARTIFACTS'], "coverage")
   SimpleCov.coverage_dir(dir)
 end
-
-SimpleCov.start
+SimpleCov.start 'rails'
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 's2_netbox'
