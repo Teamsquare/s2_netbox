881d38488ceadd35674d98531c81d01ec37db05d
Michael Shimmins <michael@teamsquare.co>
2016-11-11 20:13:57 +1100

    Updated spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'simplecov'
 
 if ENV['CIRCLE_ARTIFACTS']
-  dir = File.join("..", "..", "..", ENV['CIRCLE_ARTIFACTS'], "coverage")
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
   SimpleCov.coverage_dir(dir)
 end
 
